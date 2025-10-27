c848f86a95a9925b2d0617e348b2659920be7afc
Leandro <saavedraexequiel22@gmail.com>
2025-10-27 17:22:23 -0300

    Delete trabajoPractico02/trabajoPractico02 directory

--- trabajoPractico02/trabajoPractico02/src/main/java/ar/org/TrabajoPractico02/curso/java/trabajoPractico02/test/TestStatic.java
+++
@@ -1,8 +0,0 @@
-private static list<Vehicuo> cargarVehiculos() {
-        List<Vehiculo> vehiculos = new ArrayList<>();
-        vehiculos.add(new Auto("Peugeot", "206", 200000.0, 4));
-        vehiculos.add(new Moto("Honda", "Titan", 60000.0, 125));
-        vehiculos.add(new Auto("Peugeot", "208", 250000.0, 5));
-        vehiculos.add(new Moto("Yamaha", "YBR", 80500.50, 160));
-        return vehiculos;
-    }
